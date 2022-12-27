e7974c5a5d77225ce66fda623f3f748c9e8a3862
= <=>
2022-12-27 20:45:46 +0530

    phonebook added

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-import random
-class GenerateSecret : 
-    def __init__(self) : 
-        secret = ''
-        pattern = 'W&&FIW&&FI#2J09-2#73**R9E#7d@sf(ksd$kfm2^37NMNJSJNDFUWUIDQBI'
-        choice = int(input("Enter length for your secret\n"))
-        for data in range(choice) : 
-            limit = len(pattern)-1
-            index = random.randint(0,limit)
-            secret = secret+pattern[index]
-        print(secret,'\n')
-        Main()
-
-class Exit : 
-    def __init__(self) : 
-        print("Good Bye !")
-        quit()
-
-class NotFound : 
-    def __init__(self) : 
-        print("Option not found !\n")
-        Main()
+from src.GenerateSecret import GenerateSecret
+from src.Exit import Exit
+from src.NotFound import NotFound
+from src.Phonebook import Phonebook
 
 class Main : 
     def __init__(self) : 
         print("Welcome Back !")
         print("App Menu")
         print("1. Generate Secret")
-        print("2. Exit")
+        print("2. Phonebook")
+        print("3. Exit")
         option = int(input("Choose an option\n"))
-        if option == 1 : GenerateSecret()
-        elif option == 2 : Exit()
-        else : NotFound()
+        if option == 1 : 
+            GenerateSecret()
+            Main()
+        elif option == 2 : 
+            Phonebook()
+            Main()
+        elif option == 3 : 
+            Exit()
+        else : 
+            NotFound()
+            Main()
 Main()
